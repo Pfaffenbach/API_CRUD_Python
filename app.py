90939d69296164db0fef5e1535d8ced696568ba6
Erich Pfaffenbach <49520647+Pfaffenbach@users.noreply.github.com>
2022-03-26 17:31:32 -0300

    Select ALL
    Incluindo função Select ALL e padronizando a saída

--- app.py
+++ app.py
@@ -10,3 +10,29 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = conexao
 db = SQLAlchemy(app)
+
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key= True)
+    nome = db.Column(db.String(50))
+    email = db.Column(db.String(100))
+
+    def to_json(self):
+        return {"id": self.id, "nome": self.nome, "email": self.email}
+
+#Funcção criada para obter resultado de maneira mais organizado utilizando Json
+def gera_response(status, nome_do_conteudo, conteudo, mensagem=False):
+    body = {}
+    body[nome_do_conteudo] = conteudo
+
+    if(mensagem):
+        body["mensagem"] = mensagem
+
+    return Response(json.dumps(body), status=status, mimetype="application/json")
+
+# Selecionar Tudo
+@app.route("/usuarios", methods=["GET"])
+def seleciona_usuarios():
+    usuarios_objetos = Usuario.query.all()
+    usuarios_json = [usuario.to_json() for usuario in usuarios_objetos]
+
+    return gera_response(200, "usuarios", usuarios_json)

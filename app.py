4f3a5294addce87d322667f3d46dfac23b324bfd
Erich Pfaffenbach <49520647+Pfaffenbach@users.noreply.github.com>
2022-03-26 17:28:20 -0300

    Banco de Dados
    Fazendo conexão com banco de dados

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask, Response, Request
+from flask import Flask, Response, request
 from flask_sqlalchemy import SQLAlchemy
-
 import mysql.connector
 import json
-import email
 
+#Conexao com banco de dados MySQL Workbench
+conexao = mysql.connector.connect(host = 'localhost', database = 'youtube', user = 'root', password = 'Ee.46829308')
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = conexao
+db = SQLAlchemy(app)
